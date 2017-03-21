a44454a27c0f038643672f3cbf4172c21263d67c
ojhermann-ucd <otto.hermann@ucdconnect.ie>
2017-03-21 21:50:39 +0000

    Cleaned up comments in getData.py

--- JSON/getData.py
+++ JSON/getData.py
@@ -1,25 +1,3 @@
-# # imports
-# import dbGet as db
-# import owGet as ow
-
-# myData = db.dbData("dbAPI.txt")
-
-# import time
-
-# for d in myData:
-	
-# 	# latitude and longitude
-# 	lat = d['position']['lat']
-# 	lon = d['position']['lng']
-	
-# 	# Open Weather Data
-# 	myWeather = ow.owData("owAPI.txt", lat, lon)
-# 	time.sleep(1) # Open Weather only allows 60 calls per minute; making sure we adhere to this limit
-
-# 	# combine the data
-# 	d['ow'] = myWeather
-# 	print(d)
-
 def combineData(dbAPI, owAPI):
 
 	# imports
